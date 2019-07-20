7dbd22a143f8675a7fbcca86708f6548310b75b1
Mohamed El Ghamry <mohamed.elghamry@outlook.com>
2019-07-20 14:53:24 +0200

    ECMA6 changes
    Signed-off-by: Mohamed El Ghamry <mohamed.elghamry@outlook.com>

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-var http    = require('http'), 
+const http    = require('http'), 
     io      = require('socket.io'),
     sys     = require('sys'),
     express = require('express');
 
-var port = 8111;
+const port = 8111;
 
-//Upgraded for express 3.x
-var app = express();
+const app = express();
 app.use(express.static(__dirname + '/public'));
 // app.use(express.errorHandler({showStack: true, dumpExceptions: true}));
 
 //socket requires a http server
-var socket = io.listen(http.createServer(app).listen(port));
+const socket = io.listen(http.createServer(app).listen(port));
 sys.log('Started server on http://localhost:' + port + '/')
 
 socket.sockets.on('connection', function(client){
-  var connected = true;
+  let connected = true;
 
   //On receiving the message event - echo to console
   client.on('message', function(m){
@@ -27,12 +26,12 @@ socket.sockets.on('connection', function(client){
   });
 
   //Loop function that sends the current date time
-  var tick = function(){
+  const tick = function(){
     if (!connected) {
       return;
     }
 
-    var dateTime = new Date();
+    const dateTime = new Date();
     
     console.log("Sending " + dateTime);
     
